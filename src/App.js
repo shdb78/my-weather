5670ae97db8c5f38bc26a0c65249d7276b7d84cb
NoName <noemail@mail.com>
2023-06-27 20:06:21 +0500

    mod_1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*class Button extends React.Component {
+render() {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+}*/
+
+function Metka() {
+  return (
+    <div style = {{
+      textAlign: '20px',
+      border: '3px solid red',
+      padding: '7px',
+      width: '300px'
+    }}>
+      Поиск      
+    </div>
+  )
+}
+
+function ButtonSearch() {
+  return (
+    <div> 
+      <button style = {{
+        color: '#ff00ff',
+        height: '40px',
+        width: '100px'
+      }}> 
+        Прогноз
+      </button>
+    </div>
+  )
+}
+
+function NameApp() {
+  return (
+    <div style = {{
+      fontSize: '50px',
+      textAlign: 'center',
+      left: '350px'
+    }}>
+      Прогноз погоды
+    </div>
+  )
+}
+
+function MyWeather() {
+  return (
+    <div style = {{
+      textAlign: 'center',
+      border: '3px solid black',
+      padding: '1px',
+      width: '600px',
+      height: '600px'
+    }}>
+      Результат      
+    </div>
+  )
+}
+
 function App() {
+  return (
+    <div ButtonSearch style = {{left: '300px'}} >
+      <NameApp/>
+      <Metka/>
+      <ButtonSearch/>
+      <MyWeather/>
+    </div>
+  )
+}
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +94,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
